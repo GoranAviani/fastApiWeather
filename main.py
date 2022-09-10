0524f90ff9618b80389258728bfd86d35b27478c
goranaviani <goranaviani@gmail.com>
2022-09-10 12:56:48 +0200

    setup middleware

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "http://localhost:3000",
+]
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # PATH=/bin:/usr/bin:/usr/local/bin:${PATH}
 
 @app.get("/")
